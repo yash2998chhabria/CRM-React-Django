c3e106d917919e03ab924820f85a6328de59456f
shashank42069 <shashankgarad777@gmail.com>
2020-07-26 18:32:09 +0530

    djrect CRUD

--- djreact/backendd/djreact/articles/api/urls.py
+++ djreact/backendd/djreact/articles/api/urls.py
@@ -1,7 +1,14 @@
-from .views import ArticleListView , ArticleDetailView
+from .views import ArticleView 
 from django.urls import path,include
 
-urlpatterns = [
-    path('',ArticleListView.as_view()),
-    path('<pk>', ArticleDetailView.as_view()),
-]
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('', ArticleView, basename='userviewset')
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('',ArticleListView.as_view()),
+#     path('<pk>', ArticleDetailView.as_view()),
+# ]
--- djreact/backendd/djreact/articles/api/views.py
+++ djreact/backendd/djreact/articles/api/views.py
@@ -1,13 +1,24 @@
 from rest_framework.generics import ListAPIView,RetrieveAPIView
 from .serializers import ArticleSerializer
 from articles.models import Article
+from rest_framework import viewsets
 
 
-class ArticleListView(ListAPIView):
-    queryset = Article.objects.all()
+
+
+class ArticleView(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
     serializer_class = ArticleSerializer
-
-
-class ArticleDetailView(RetrieveAPIView):
     queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
+
+
+# class ArticleListView(ListAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
+
+
+# class ArticleDetailView(RetrieveAPIView):
+#     queryset = Article.objects.all()
+#     serializer_class = ArticleSerializer
